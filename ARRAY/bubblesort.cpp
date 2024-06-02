46c76dd203011a7bae5a89d97ba1d7124b253ed0
divakarkumar2000 <divakarkumar2000@gmail.com>
2024-06-02 19:13:26 +0530

    Some changes 2

--- ARRAY/bubblesort.cpp
+++ ARRAY/bubblesort.cpp
@@ -2,17 +2,7 @@
 /* In bubble sort , we just have to take heaviest element to the bottom and lighter bubble up */
 /*The simple steps of achieving the bubble sort are listed as follows -
 
-Step 1 - If the element is the first element, assume that it is already sorted. Return 1.
-
-Step2 - Pick the next element, and store it separately in a key.
-
-Step3 - Now, compare the key with all elements in the sorted array.
-
-Step 4 - If the element in the sorted array is smaller than the current element, then move to the next element. Else, shift greater elements in the array towards the right.
-
-Step 5 - Inserst the value.
-
-Step 6 - Repeat until the array is sorted.
+1. Our motive to put the heaviest element in the end of the array . Use swap
 
 Adaptive -> T.C. O(N^2) 
             Best O(N)
